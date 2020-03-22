4f5884e7ba107e0b525459f12b9516e909238f87
Kidus Kinfe <53956331+KidusY@users.noreply.github.com>
2020-03-22 14:25:14 -0700

    first commit

---
+++ precache-manifest.3744fe586ef93c279c5b99f3131bfae0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ffe46ec60a761a92abc78a0fda95ca",
+    "url": "/Monsters-ReactApp/index.html"
+  },
+  {
+    "revision": "bcfe62057b1476c8dd07",
+    "url": "/Monsters-ReactApp/static/css/main.54e337ef.chunk.css"
+  },
+  {
+    "revision": "3db869bb7de2277be351",
+    "url": "/Monsters-ReactApp/static/js/2.e603f8d1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Monsters-ReactApp/static/js/2.e603f8d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcfe62057b1476c8dd07",
+    "url": "/Monsters-ReactApp/static/js/main.e40a3d0a.chunk.js"
+  },
+  {
+    "revision": "b5fcde08f2ac6373c753",
+    "url": "/Monsters-ReactApp/static/js/runtime-main.811318c7.js"
+  }
+]);
